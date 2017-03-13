0d558470c2e562b419d8d95be1bf9726a7bfe5e7
Mirjam Pergar <mirjam.pergar@gmail.com>
2017-03-13 19:03:01 +0100

    Napisala kodo in poslala vprašanje mentorju

--- primercek.R
+++ primercek.R
@@ -1,10 +1,10 @@
 n <- 2;
-r <- 2;
+r <- 3;
 F <- c();
 i <- 1;
 
 time0 <- proc.time()
-while(i<=50000){
+while(i<=10000){
 #vzorca
 X_12 <- rnorm(n)
 X_22 <- rnorm(n)
@@ -18,9 +18,9 @@ F[i] <- round(((1/r)*S[1]+(3/r)*S[2])/(S_12+S_22),2)
 i <- i+1;
 }
 
-pdf('Primer_50000.pdf')
+pdf('Primer_10000.pdf')
 time <- proc.time()-time0
 x<-seq(0,10,0.01);
-hist(F, freq = FALSE, xlim = c(0,2),ylim = c(0,5))
+hist(F, freq = FALSE, xlim = c)
 curve(dnorm(x,mean=mean(F),sd=sd(F)),add=TRUE, col = 'red')
 dev.off()
--- test.R
+++ test.R
@@ -1,20 +1,36 @@
-n <- 10;
-r <- 2;
+n <- 2
+r <- 3;
 i <- 1;
 F <- c();
+ponovitve <- 10000;
 
-while(i<=10){
+time0 <- proc.time()
+
+while(i<=ponovitve){
 
 X <- matrix(0,nrow = r, ncol = n);
 S <- c();
+F1 <- c();
 
 for(j in 1:r){
-  X[j,]<- rnorm(n)
+  X[j,]<- rnorm(n);
+  vsota = 0;
+  
   for(k in 1:n){
-    S[j] <- sqrt((1/(n-1))*sum((X[j,k]-mean(X[j,]))^2))
+    vsota = vsota + (X[j,k]-mean(X[j,]))^2; #vsota pri cenilki variance
   }
+  S[j] <- sqrt((1/(n-1))*vsota);
+  F1[j] <- (2*j -1)/r; #uteÅ¾i
 }
-SS <- sort(S)
+
+SS <- sort(S); #vrstilne stat
+F[i] <- round(sum(SS*F1)/sum(S),2);
 i <- i+1;
-#Kako napišeš F vsoto po i?
+
 }
+time <- proc.time()-time0
+#pdf('Primer_10000.pdf')
+x<-seq(0,10,0.01);
+hist(F, freq = FALSE,xlim = c(min(F)-0.2,max(F)+0.2))
+curve(dnorm(x,mean=mean(F),sd=sd(F)),add=TRUE, col = 'red')
+#dev.off()
