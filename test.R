cc6127fb236a0792d298a48d79c32571c8cd2fcf
Mirjam Pergar <mirjam.pergar@gmail.com>
2017-04-06 23:19:57 +0200

    Generate table and beamer
    Signed-off-by: Mirjam Pergar <mirjam.pergar@gmail.com>

--- test.R
+++ test.R
@@ -1,26 +1,27 @@
 library(matrixStats)
 
 
-N <- c(seq(2,10),seq(12,20,2),25,30,40,60);
-R <- c(seq(2,10),15,20);
+N <- c(seq(12,16,2))#,25,30,40,60);
+R <- c(20)#,20);
 alpha <- 0.01;
-alpha_0.01 <- data.frame(row.names = N)
+#alpha_0.01 <- matrix(as.numeric(0),nrow=length(N),ncol=length(R),dimnames = list(N,R))
 
 loops <- 10^6;
-time0 <- proc.time() #we start the timer
+# time0 <- proc.time() #we start the timer
 for(r in R){
+  print(r)
   for(n in N){
+    print(n)
     rr <- seq(1,r);
     X <- matrix(rnorm(n*r*loops),nrow = r*loops,ncol=n);#generate a random matrix
     S <- rowVars(X); #row sample variances
     dim(S)<-c(loops,r);#vector -> matrix
     F <- sapply(1:loops,function(x) round(sum(sort(S[x,])*(2*rr -1) / r)/sum(S[x,]),3))
-    alpha_0.01['N','R'] <- quantile(F,1-alpha); #95% jih je levo od te meje
+    alpha_0.01[as.character(n),as.character(r)] <- quantile(F,1-alpha); #95% jih je levo od te meje
   }
 }
-
-time <- proc.time()-time0 #we stop the timer
-print(time)
+save(alpha_0.01,file="ALFA_1.csv")
+write.table(alpha_0.01, file="alfa_1.txt")
 #pdf('Primer_1e+6_1percent.pdf')
 # x<-seq(0,10,0.01);
 # hist(F,freq = FALSE,xlim = c(min(F)-0.2,max(F)+0.2))
@@ -30,3 +31,6 @@ print(time)
 
 # user  system elapsed 
 # 14.95    0.04   15.06
+
+# user  system elapsed 
+# 2021.92   30.10 2123.33 
