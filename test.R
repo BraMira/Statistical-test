be7f1aaf80a19a1ac04fa8538d0d0ac5c7141234
Mirjam Pergar <mirjam.pergar@gmail.com>
2017-05-20 14:50:32 +0200

    ne vem....

--- test.R
+++ test.R
@@ -1,18 +1,18 @@
 library(matrixStats)
 library(bigmemory)
 
-alpha_0.01 <- read.table("alfa_1.txt",header=TRUE,row.names = 1)
-colnames(alpha_0.01)<-c(seq(2,10),15,20)
+# alpha_0.01 <- read.table("alfa_1.txt",header=TRUE,row.names = 1)
+# colnames(alpha_0.01)<-c(seq(2,10),15,20)
 # N <- c(seq(12,16,2))#,25,30,40,60);
 # R <- c(20)#,20);
 alpha <- 0.01;
 #alpha_0.01 <- matrix(as.numeric(0),nrow=length(N),ncol=length(R),dimnames = list(N,R))
 
-loops <- 10^6;
+loops <- 10^7;
 # time0 <- proc.time() #we start the timer
 
-n <- 40;
-r <- 10;
+n <- 60;
+r <- 2;
 
 
 rr <- seq(1,r);
@@ -22,15 +22,15 @@ X[,]<-rnorm(n*r*loops);
 S <- rowVars(as.matrix(X)); #row sample variances
 dim(S)<-c(loops,r);#vector -> matrix
 F <- sapply(1:loops,function(x) round(sum(sort(S[x,])*(2*rr -1) / r)/sum(S[x,]),3));
-alpha_0.01[as.character(n),as.character(r)] <- quantile(F,1-alpha); #95% jih je levo od te meje
-
-save(alpha_0.01,file="ALFA_1.csv")
-write.table(alpha_0.01, file="alfa_1.txt")
+# alpha_0.01[as.character(n),as.character(r)] <- quantile(F,1-alpha); #95% jih je levo od te meje
+# 
+# save(alpha_0.01,file="ALFA_1.csv")
+# write.table(alpha_0.01, file="alfa_1.txt")
 
 #pdf('Primer_1e+6_1percent.pdf')
-# x<-seq(0,10,0.01);
-# hist(F,freq = FALSE,xlim = c(min(F)-0.2,max(F)+0.2))
-# curve(dnorm(x,mean=mean(F),sd=sd(F)),add=TRUE, col = 'red')
+x<-seq(0,10,0.01);
+hist(F,freq = FALSE,xlim = c(min(F)-0.2,max(F)+0.2))
+curve(dnorm(x,mean=mean(F),sd=sd(F)),add=TRUE, col = 'red')
 # abline(v=quantile(F,0.99),col='green',lwd=2)
 # dev.off()
 
--- test1.R
+++ test1.R
@@ -1,19 +1,19 @@
 library(matrixStats)
-library(bigmemory)
+# library(bigmemory)
+# 
+# #alpha_0.01 <- read.table("alfa_1.txt",header=TRUE,row.names = 1)
+# #colnames(alpha_0.01)<-c(seq(2,10),15,20)
+# alpha_0.05 <- read.table("alfa_2.txt",header=TRUE,row.names = 1)
+# colnames(alpha_0.05)<-c(seq(2,10),15,20)
+# alpha_0.1 <- read.table("alfa_3.txt",header=TRUE,row.names = 1)
+# colnames(alpha_0.1)<-c(seq(2,10),15,20)
 
-#alpha_0.01 <- read.table("alfa_1.txt",header=TRUE,row.names = 1)
-#colnames(alpha_0.01)<-c(seq(2,10),15,20)
-alpha_0.05 <- read.table("alfa_2.txt",header=TRUE,row.names = 1)
-colnames(alpha_0.05)<-c(seq(2,10),15,20)
-alpha_0.1 <- read.table("alfa_3.txt",header=TRUE,row.names = 1)
-colnames(alpha_0.1)<-c(seq(2,10),15,20)
-
-N <- c(seq(2,10),12,14);
-R <- c(15,20);#seq(3,10);#,20);
+N <- 60;
+R <- 2;#seq(3,10);#,20);
 #alpha <- 0.01;
 #alpha_0.01 <- matrix(as.numeric(0),nrow=length(N),ncol=length(R),dimnames = list(N,R))
 
-loops <- 10^6;
+loops <- 10^7;
 # time0 <- proc.time() #we start the timer
 for(r in R){
   print(r)
@@ -28,13 +28,17 @@ for(r in R){
     dim(S)<-c(loops,r);#vector -> matrix
     F <- sapply(1:loops,function(x) round(sum(sort(S[x,])*(2*rr -1) / r)/sum(S[x,]),3));
     #alpha_0.01[as.character(n),as.character(r)] <- quantile(F,1-0.01);
-    alpha_0.05[as.character(n),as.character(r)] <- quantile(F,1-0.05);
-    alpha_0.1[as.character(n),as.character(r)] <- quantile(F,1-0.1);
-    write.table(alpha_0.05, file="alfa_2.txt")
-    write.table(alpha_0.1, file="alfa_3.txt")
+    # alpha_0.05[as.character(n),as.character(r)] <- quantile(F,1-0.05);
+    # alpha_0.1[as.character(n),as.character(r)] <- quantile(F,1-0.1);
+    # write.table(alpha_0.05, file="alfa_2.txt")
+    # write.table(alpha_0.1, file="alfa_3.txt")
   }
 }
 #save(alpha_0.01,file="ALFA_1.csv")
 #write.table(alfa_0.01, file="alfa_1.txt")
-write.table(alpha_0.05, file="alfa_2.txt")
-write.table(alpha_0.1, file="alfa_3.txt")
+# write.table(alpha_0.05, file="alfa_2.txt")
+# write.table(alpha_0.1, file="alfa_3.txt")
+
+x<-seq(0,10,0.01);
+hist(F,freq = FALSE,xlim = c(min(F)-0.2,max(F)+0.2))
+curve(dnorm(x,mean=mean(F),sd=sd(F)),add=TRUE, col = 'red')
